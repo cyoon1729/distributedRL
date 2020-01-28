trying to fix memory leakage; move all remote calls to central training loop

Apex-DQN/actor.py
@@ -7,15 +7,18 @@ import gym
 import numpy as np 
 
 from collections import deque
-from myutils import to_n_step, toTensor
+from myutils import to_n_step, toTensor, Buffer
 from models import create_learner
 
 
 @ray.remote
 class Actor(object):
 
-    def __init__(self, worker_id, config, save_dir, buffer_remote, param_server):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self,
+                 worker_id,
+                 config,
+                 save_dir):
+        self.device = config['actor_device']
         self.worker_id = worker_id
 
         env_id = config['env']
@@ -23,6 +26,8 @@ class Actor(object):
         self.unroll_cnt = config['unroll_steps']
         self.num_train_step = config['num_train_steps']
         self.max_ep_length = config['max_ep_length']
+        self.actor_buffer_size = config['actor_buffer_size']
+        self.local_buffer = Buffer(self.actor_buffer_size)
 
         self.env = gym.make(env_id)
         random.seed(worker_id+random_seed)
@@ -30,17 +35,17 @@ class Actor(object):
 
         self.use_distributional = config['use_distributional']
         self.q_network = create_learner(config['network_type'],
-            config['obs_dim'], config['action_dim'], config['num_quantiles'])
+                                        config['obs_dim'],
+                                        config['action_dim'],
+                                        config['num_quantiles'])
         self.eps = 0.2 # epsilon-greedy exploration
 
-        # parallelization
-        self.buffer_remote = buffer_remote
-        self.param_server = param_server
-
         # logging
+        self.actor_save_interval = config['actor_save_interval']
         self.save_dir = save_dir
+        self.episode = 0
+        self.update_step = 0
         self.log = list()
-
 
     def sample(self, state):
         state = torch.FloatTensor(state).unsqueeze(0)
@@ -55,57 +60,78 @@ class Actor(object):
         else:
             return self.env.action_space.sample()
 
-    def sync_with_param_server(self):
-        new_params = ray.get(self.param_server.return_params.remote())
+    def set_params(self, new_params):
         for param, new_param in zip(self.q_network.parameters(), new_params):
-            new_param = torch.Tensor(new_param).to(self.device)
+            new_param = torch.FloatTensor(new_param).to(self.device)
             param.data.copy_(new_param)
 
-    def run(self):
-        time.sleep(3)
+    def start(self):
+        print("Starting actor-{0}".format(self.worker_id))
+        os.makedirs("{0}/agent-{1}".format(self.save_dir, self.worker_id))
+        self.fill_buffer(0, 100)
+
+    def fill_buffer(self, update_step=0, buffer_additions=None):
+        if buffer_additions is None:
+            buffer_additions = self.actor_buffer_size
+        self.update_step = update_step
         transition_buffer = deque(maxlen=self.unroll_cnt + 1)
         state = self.env.reset()
-        episode_reward = 0 
-        episode = 0
-        ep_length = 0
-        update_step = ray.get(self.param_server.get_update_step.remote())
-        
-        while update_step < self.num_train_step:
-            ep_length += 1
+        episode_reward = 0
+        transitions_added = 0
+
+        while transitions_added < buffer_additions:
             action = self.sample(state)  
             next_state, reward, done, _ = self.env.step(action)  
-            transition_buffer.append((state, action, reward, done))
-            episode_reward += reward            
+            transition = (state, action, reward, done)
+            transition_buffer.append(transition)
+            episode_reward += reward        
 
             if len(transition_buffer) == self.unroll_cnt + 1:
-                self.buffer_remote.add.remote(*to_n_step(transition_buffer))
+                self.local_buffer.add(*to_n_step(transition_buffer))
+                transitions_added += 1
 
-            if done or ep_length == self.max_ep_length:
+            if done:
                 # add to log
-                logging = {'update_step': update_step,
-                    'episode': episode, 'episode reward': episode_reward}
+                logging = {'update_step': self.update_step,
+                           'episode': self.episode,
+                           'episode reward': episode_reward}
                 self.log.append(logging)
-
-                # print results locally
-                print(f"Episode {episode}: {episode_reward}")
+                
+                if self.episode % 100 == 0:
+                    print("Actor-{0}, episode {1} : {2}".format(
+                            self.worker_id, self.episode, episode_reward))
 
                 # prepare new rollout
-                episode += 1
+                self.episode += 1
                 episode_reward = 0
-                ep_length = 0
                 transition_buffer.clear()
                 next_state = self.env.reset()
-            
-            if update_step % 3 == 0:
-                self.sync_with_param_server()
-            
+                
+            if self.episode % self.actor_save_interval == 0:
+                save_dir = "{0}/agent-{1}/agent-{1}-rewards.npy".format(
+                               self.save_dir, self.worker_id)
+                np.save(save_dir, 
+                        np.asarray(self.log))
+                           
             state = next_state
-            update_step = ray.get(self.param_server.get_update_step.remote())
-        
+
+    def return_buffer(self):
+        return self.local_buffer.return_buffer()
+
+    def final_save(self):
         # save results in result file
-        print(f"Saving actor-{self.worker_id} data...")
-        os.makedirs(f'{self.save_dir}/agent-{self.worker_id}')
-        with open(f'{self.save_dir}/agent-{self.worker_id}/log.pkl', 'wb') as f:
+        print("Saving actor-{self.worker_id} data...".format(self.worker_id))
+        
+        file_dir = "{0}/agent-{1}/log.pkl".format(
+            self.save_dir, 
+            self.worker_id)
+
+        with open(file_dir, 'wb') as f:
             pickle.dump(self.log, f)
 
-        print(f"Actor-{self.worker_id} exit")
+        print("plotting actor data")
+        plot_agent(
+            "{0}/agent-{1}/log.pkl".format(self.save_dir, self.worker_id),
+            "{0}/agent-{1}".format(self.save_dir, self.worker_id))
+
+        print("Actor-{0} exit".format(self.worker_id))

Apex-DQN/learner.py
@@ -12,9 +12,9 @@ from myutils import huber
 @ray.remote
 class Learner:
 
-    def __init__(self, config, buffer_remote, param_server, save_dir):
+    def __init__(self, config, save_dir):
         # read config 
-        self.device = torch.device(config['device'])
+        self.device = torch.device(config['learner_device'])
         self.num_train_steps = config['num_train_steps']   
 
         # environment_info
@@ -25,11 +25,6 @@ class Learner:
         # buffer info
         self.use_per = config['use_per']
         self.priority_alpha = config['priority_alpha']
-        self.priority_beta_start = config['priority_beta_start']
-        self.priority_beta_end = config['priority_beta_end']
-        self.priority_beta = self.priority_beta_start
-        self.priority_beta_increment = ((self.priority_beta_end - self.priority_beta_start)
-                                        / self.num_train_steps)
         self.batch_size = config['batch_size']
 
         # hyperparameters
@@ -41,10 +36,13 @@ class Learner:
         num_quantiles = config['num_quantiles']
 
         # initialize networks
-        self.q_network = create_learner(network_type, obs_dim, action_dim)
-        self.target_q_network = create_learner(network_type, obs_dim, action_dim)
+        self.q_network = create_learner(network_type,
+                                        obs_dim,
+                                        action_dim)
+        self.target_q_network = create_learner(network_type,
+                                               obs_dim,
+                                               action_dim)
         self.param_list = list(self.q_network.state_dict())
-        self.param_server = param_server
 
         for target_param, param in zip(self.q_network.parameters(),
                                        self.target_q_network.parameters()):
@@ -53,13 +51,12 @@ class Learner:
         # initialize optimizers
         self.q_optimizer = optim.Adam(self.q_network.parameters(), lr=q_lr)
     
-        # parallelization
-        self.buffer_remote = buffer_remote
-        self.update_step = 0
-
         # logging
+        self.update_step = 0 
         self.save_dir = save_dir
         self.log = list()
+        os.makedirs("{0}/learner".format(self.save_dir))
+        os.makedirs("{0}/learner/models".format(self.save_dir))
 
     def optimize_parameters(self, batch, weights, eps=1e-4):
         states, actions, rewards, last_states, dones = batch
@@ -73,7 +70,8 @@ class Learner:
             dist = self.q_network.forward(states)
             dist = dist[torch.arange(dist.size(0)), actions.view(-1)]
             bootstrap_q = self.target_q_network.forward(last_states)
-            bootstrap_q_max = torch.max(bootstrap_q.mean(dim=2), 1, keepdim=True)[0]
+            bootstrap_q_max = torch.max(
+                bootstrap_q.mean(dim=2), 1, keepdim=True)[0]
             q_targets = (rewards + (1 - dones) *
                         self.gamma**self.unroll_steps * bootstrap_q_max)
 
@@ -114,56 +112,53 @@ class Learner:
 
         return q_loss, td_error
     
-    def update_param_server(self):
+    def return_param_list(self):
+        return self.param_list
+    
+    def return_numpy_policy(self):
+        # return numpy state_dict
         params = []
         state_dict = self.q_network.state_dict()
         for param in self.param_list:
-            params.append(state_dict[param].numpy())
-        self.param_server.update_params.remote(params)
-    
-    def return_param_list(self):
-        return self.param_list
+            params.append(state_dict[param].cpu().numpy())
+        
+        return params 
 
     def return_update_step(self):
         return self.update_step
+
+    def learning_step(self, batch, indices, weights):
+        step_info = list(self.optimize_parameters(batch, weights))
+        if self.use_per:
+            _, td_error = step_info
+            new_priorities = td_error.detach().squeeze().abs()
+            new_priorities = new_priorities.cpu().numpy().tolist() 
+        else:
+            None
+        self.update_step += 1
+
+        return step_info, indices, new_priorities
     
-    def run(self):
-        while ray.get(self.buffer_remote.get_size.remote()) < self.batch_size:
-            continue
+    def logger(self, step_info):
+        q_loss, td_error = step_info
+        logging = {
+            'update_step': self.update_step,
+            'q_loss': q_loss.detach().numpy(),
+            'td_error': td_error.detach().numpy(),
+        }
+        self.log.append(logging)
 
-        while self.update_step < self.num_train_steps:
-            # fetch batch
-            if self.use_per:
-                batch, indices, weights = ray.get(
-                    self.buffer_remote.sample.remote(self.batch_size, self.priority_beta))
-                self.priority_beta += self.priority_beta_increment
-            else:
-                batch = ray.get(self.buffer_remote.sample.remote(self.batch_size))
-                weights = None   # weights are set to none because no prioritized replay
-
-            # update parameters
-            q_loss, td_error = self.optimize_parameters(batch, weights)
-
-            # update prioritized for prioritized experience replay
-            if self.use_per:
-                new_priorities = td_error.detach().squeeze().abs().cpu().numpy().tolist() 
-                self.buffer_remote.update_priorities.remote(indices, new_priorities)
-
-            # log
-            logging = {'update_step': self.update_step, 'q_loss': q_loss, 'td_error': td_error}
-            self.log.append(logging)
-
-            # sync with global
-            self.update_param_server()
-            self.update_step += 1
-
-            if self.update_step % 100 == 0:
-                print(f"learner update step: {self.update_step}")
-
-        # save results in result file
-        print("Saving learner data...")
-        os.makedirs(f'{self.save_dir}/learner')
-        with open(f'{self.save_dir}/learner/log.pkl', 'wb') as f:
-            pickle.dump(self.log, f)
-
-        print("Learner exit")
+        if self.update_step % 100 == 0:
+            learner_state = {
+                'epoch': self.update_step,
+                'q_state_dict': self.q_network.state_dict(),
+                'q_optimizer': self.q_optimizer.state_dict(),
+            }
+            torch.save(
+                learner_state,
+                "{0}/learner/models/learner_state-{1}.pth".format(
+                    self.save_dir,
+                    self.update_step)
+            )
+            
+            print("learner update step: {0}".format(self.update_step))

Apex-DQN/myutils.py
@@ -78,7 +78,7 @@ class ParameterServer(object):
         return self.update_step
 
 
-@ray.remote
+
 class Buffer(object):
     
     def __init__(self, max_size):
@@ -104,25 +104,17 @@ class Buffer(object):
     def return_buffer(self):
         return self.buffer
 
-
 @ray.remote
 class PrioritizedReplayBuffer(object):
     def __init__(self, size, alpha):
-        """Create Prioritized Replay buffer.
-        Parameters
-        ----------
-        size: int
-            Max number of transitions to store in the buffer. When the buffer
-            overflows the old memories are dropped.
-        alpha: float
-            how much prioritization is used
-            (0 - no prioritization, 1 - full prioritization)
-        See Also
-        --------
-        ReplayBuffer.__init__
+        """
+        Create Prioritized Replay buffer.
+        See Also ReplayBuffer.__init__
+        :param size: (int) Max number of transitions to store in the buffer. When the buffer overflows the old memories
+            are dropped.
+        :param alpha: (float) how much prioritization is used (0 - no prioritization, 1 - full prioritization)
         """
         #super(PrioritizedReplayBuffer, self).__init__(size)
-
         self._storage = []
         self._maxsize = size
         self._next_idx = 0
@@ -130,24 +122,52 @@ class PrioritizedReplayBuffer(object):
         assert alpha >= 0
         self._alpha = alpha
 
-        self.it_capacity = 1
-        while self.it_capacity < size * 2:  # We use double the soft capacity of the PER for the segment trees to allow for any overflow over the soft capacity limit before samples are removed
-            self.it_capacity *= 2
+        it_capacity = 1
+        while it_capacity < size:
+            it_capacity *= 2
 
-        self._it_sum = SumSegmentTree(self.it_capacity)
-        self._it_min = MinSegmentTree(self.it_capacity)
+        self._it_sum = SumSegmentTree(it_capacity)
+        self._it_min = MinSegmentTree(it_capacity)
         self._max_priority = 1.0
-
-    def _add(self, obs_t, action, reward, obs_tp1, done): # self, state, policy_output, reward, last_state, done
+    
+    def _add(self, obs_t, action, reward, obs_tp1, done):
+        """
+        add a new transition to the buffer
+        :param obs_t: (Union[np.ndarray, int]) the last observation
+        :param action: (Union[np.ndarray, int]) the action
+        :param reward: (float) the reward of the transition
+        :param obs_tp1: (Union[np.ndarray, int]) the current observation
+        :param done: (bool) is the episode done
+        """
         data = (obs_t, action, reward, obs_tp1, done)
 
-        self._storage.append(data)
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
 
-        self._next_idx += 1
-    
-    def _remove(self, num_samples):
-        del self._storage[:num_samples]
-        self._next_idx = len(self._storage)
+    def add(self, obs_t, action, reward, obs_tp1, done):
+        """
+        add a new transition to the buffer
+        :param obs_t: (Any) the last observation
+        :param action: ([float]) the action
+        :param reward: (float) the reward of the transition
+        :param obs_tp1: (Any) the current observation
+        :param done: (bool) is the episode done
+        """
+        idx = self._next_idx
+        self._add(obs_t, action, reward, obs_tp1, done)
+        self._it_sum[idx] = self._max_priority ** self._alpha
+        self._it_min[idx] = self._max_priority ** self._alpha
+
+    def _sample_proportional(self, batch_size):
+        mass = []
+        total = self._it_sum.sum(0, len(self._storage) - 1)
+        # TODO(szymon): should we ensure no repeats?
+        mass = np.random.random(size=batch_size) * total
+        idx = self._it_sum.find_prefixsum_idx(mass)
+        return idx
 
     def _encode_sample(self, idxes):
         obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
@@ -159,105 +179,238 @@ class PrioritizedReplayBuffer(object):
             rewards.append(reward)
             obses_tp1.append(np.array(obs_tp1, copy=False))
             dones.append(done)
+        
+        sample = [
+            np.array(obses_t),
+            np.array(actions),
+            np.array(rewards),
+            np.array(obses_tp1),
+            np.array(dones)
+        ]
 
-        return [np.array(obses_t), actions, np.array(rewards),\
-                np.array(obses_tp1), np.array(dones)]
+        return sample
 
-    def add(self, state, policy_output, reward, last_state, done): # self, state, policy_output, reward, last_state, done
-        idx = self._next_idx
-        assert idx < self.it_capacity, "Number of samples in replay memory exceeds capacity of segment trees. Please increase capacity of segment trees or increase the frequency at which samples are removed from the replay memory"
-
-        self._add(state, policy_output, reward, last_state, done)
-        self._it_sum[idx] = self._max_priority ** self._alpha
-        self._it_min[idx] = self._max_priority ** self._alpha
-
-    def remove(self, num_samples):
-        self._remove(num_samples)
-        self._it_sum.remove_items(num_samples)
-        self._it_min.remove_items(num_samples)
-
-    def _sample_proportional(self, batch_size):
-        res = []
-        p_total = self._it_sum.sum(0, len(self._storage) - 1)
-        every_range_len = p_total / batch_size
-        for i in range(batch_size):
-            mass = random.random() * every_range_len + i * every_range_len
-            idx = self._it_sum.find_prefixsum_idx(mass)
-            res.append(idx)
-        return res
-
-    def sample(self, batch_size, beta, epsilon=1e-8):
-        """Sample a batch of experiences.
+    def sample(self, batch_size, beta=0):
+        """
+        Sample a batch of experiences.
         compared to ReplayBuffer.sample
         it also returns importance weights and idxes
         of sampled experiences.
-        Parameters
-        ----------
-        batch_size: int
-            How many transitions to sample.
-        beta: float
-            To what degree to use importance weights
-            (0 - no corrections, 1 - full correction)
-        Returns
-        -------
-        obs_batch: np.array
-            batch of observations
-        act_batch: np.array
-            batch of actions executed given obs_batch
-        rew_batch: np.array
-            rewards received as results of executing act_batch
-        next_obs_batch: np.array
-            next set of observations seen after executing act_batch
-        done_mask: np.array
-            done_mask[i] = 1 if executing act_batch[i] resulted in
-            the end of an episode and 0 otherwise.
-        gammas: np.array
-            product of gammas for N-step returns
-        weights: np.array
-            Array of shape (batch_size,) and dtype np.float32
-            denoting importance weight of each sampled transition
-        idxes: np.array
-            Array of shape (batch_size,) and dtype np.int32
-            idexes in buffer of sampled experiences
+        :param batch_size: (int) How many transitions to sample.
+        :param beta: (float) To what degree to use importance weights (0 - no corrections, 1 - full correction)
+        :return:
+            - obs_batch: (np.ndarray) batch of observations
+            - act_batch: (numpy float) batch of actions executed given obs_batch
+            - rew_batch: (numpy float) rewards received as results of executing act_batch
+            - next_obs_batch: (np.ndarray) next set of observations seen after executing act_batch
+            - done_mask: (numpy bool) done_mask[i] = 1 if executing act_batch[i] resulted in the end of an episode
+                and 0 otherwise.
+            - weights: (numpy float) Array of shape (batch_size,) and dtype np.float32 denoting importance weight of
+                each sampled transition
+            - idxes: (numpy int) Array of shape (batch_size,) and dtype np.int32 idexes in buffer of sampled experiences
         """
         assert beta > 0
 
         idxes = self._sample_proportional(batch_size)
-
         weights = []
-        p_min = self._it_min.min() / (self._it_sum.sum() + epsilon)
+        p_min = self._it_min.min() / self._it_sum.sum()
         max_weight = (p_min * len(self._storage)) ** (-beta)
-
-        for idx in idxes:
-            p_sample = self._it_sum[idx] / (self._it_sum.sum() + epsilon)
-            weight = (p_sample * len(self._storage)) ** (-beta)
-            weights.append(weight / (max_weight + epsilon))
-        weights = np.array(weights)
+        p_sample = self._it_sum[idxes] / self._it_sum.sum()
+        weights = (p_sample * len(self._storage)) ** (-beta) / max_weight
         encoded_sample = self._encode_sample(idxes)
-
-        return encoded_sample, idxes, weights
+        return tuple(list(encoded_sample) + [weights, idxes])
 
     def update_priorities(self, idxes, priorities):
-        """Update priorities of sampled transitions.
+        """
+        Update priorities of sampled transitions.
         sets priority of transition at index idxes[i] in buffer
         to priorities[i].
-        Parameters
-        ----------
-        idxes: [int]
-            List of idxes of sampled transitions
-        priorities: [float]
-            List of updated priorities corresponding to
-            transitions at the sampled idxes denoted by
-            variable `idxes`.
+        :param idxes: ([int]) List of idxes of sampled transitions
+        :param priorities: ([float]) List of updated priorities corresponding to transitions at the sampled idxes
+            denoted by variable `idxes`.
         """
         assert len(idxes) == len(priorities)
-        for idx, priority in zip(idxes, priorities):
-            assert priority > 0
-            assert 0 <= idx < len(self._storage)
-            self._it_sum[idx] = priority ** self._alpha
-            self._it_min[idx] = priority ** self._alpha
+        assert np.min(priorities) > 0
+        assert np.min(idxes) >= 0
+        assert np.max(idxes) < len(self.storage)
+        self._it_sum[idxes] = priorities ** self._alpha
+        self._it_min[idxes] = priorities ** self._alpha
 
-            self._max_priority = max(self._max_priority, priority)
-    
+        self._max_priority = max(self._max_priority, np.max(priorities))
+
     def get_size(self):
         return len(self._storage)
+    
+    def incorporate_buffer(self, buffer):
+        # elements of the buffer must be appropriately sized tuples
+        for element in buffer:
+            self.add(*element)
+
+# @ray.remote
+# class PrioritizedReplayBuffer(object):
+#     def __init__(self, size, alpha):
+#         """Create Prioritized Replay buffer.
+#         Parameters
+#         ----------
+#         size: int
+#             Max number of transitions to store in the buffer. When the buffer
+#             overflows the old memories are dropped.
+#         alpha: float
+#             how much prioritization is used
+#             (0 - no prioritization, 1 - full prioritization)
+#         See Also
+#         --------
+#         ReplayBuffer.__init__
+#         """
+#         #super(PrioritizedReplayBuffer, self).__init__(size)
+
+#         self._storage = []
+#         self._maxsize = size
+#         self._next_idx = 0
+
+#         assert alpha >= 0
+#         self._alpha = alpha
+
+#         self.it_capacity = 1
+#         while self.it_capacity < size * 2:  # We use double the soft capacity of the PER for the segment trees to allow for any overflow over the soft capacity limit before samples are removed
+#             self.it_capacity *= 2
+
+#         self._it_sum = SumSegmentTree(self.it_capacity)
+#         self._it_min = MinSegmentTree(self.it_capacity)
+#         self._max_priority = 1.0
+
+#     def _add(self, obs_t, action, reward, obs_tp1, done): # self, state, policy_output, reward, last_state, done
+#         data = (obs_t, action, reward, obs_tp1, done)
+
+#         self._storage.append(data)
+
+#         self._next_idx += 1
+    
+#     def _remove(self, num_samples):
+#         del self._storage[:num_samples]
+#         self._next_idx = len(self._storage)
+
+#     def _encode_sample(self, idxes):
+#         obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
+#         for i in idxes:
+#             data = self._storage[i]
+#             obs_t, action, reward, obs_tp1, done = data
+#             obses_t.append(np.array(obs_t, copy=False))
+#             actions.append(action)
+#             rewards.append(reward)
+#             obses_tp1.append(np.array(obs_tp1, copy=False))
+#             dones.append(done)
+        
+#         sample = [
+#             np.array(obses_t),
+#             np.array(actions),
+#             np.array(rewards),
+#             np.array(obses_tp1),
+#             np.array(dones)
+#         ]
+
+#         return sample
+
+#     def add(self, state, policy_output, reward, last_state, done): # self, state, policy_output, reward, last_state, done
+#         idx = self._next_idx
+#         assert idx < self.it_capacity, "Number of samples in replay memory exceeds capacity of segment trees. Please increase capacity of segment trees or increase the frequency at which samples are removed from the replay memory"
+
+#         self._add(state, policy_output, reward, last_state, done)
+#         self._it_sum[idx] = self._max_priority ** self._alpha
+#         self._it_min[idx] = self._max_priority ** self._alpha
+
+#     def remove(self, num_samples):
+#         self._remove(num_samples)
+#         self._it_sum.remove_items(num_samples)
+#         self._it_min.remove_items(num_samples)
+
+#     def _sample_proportional(self, batch_size):
+#         res = []
+#         p_total = self._it_sum.sum(0, len(self._storage) - 1)
+#         every_range_len = p_total / batch_size
+#         for i in range(batch_size):
+#             mass = random.random() * every_range_len + i * every_range_len
+#             idx = self._it_sum.find_prefixsum_idx(mass)
+#             res.append(idx)
+#         return res
+
+#     def sample(self, batch_size, beta, epsilon=1e-8):
+#         """Sample a batch of experiences.
+#         compared to ReplayBuffer.sample
+#         it also returns importance weights and idxes
+#         of sampled experiences.
+#         Parameters
+#         ----------
+#         batch_size: int
+#             How many transitions to sample.
+#         beta: float
+#             To what degree to use importance weights
+#             (0 - no corrections, 1 - full correction)
+#         Returns
+#         -------
+#         obs_batch: np.array
+#             batch of observations
+#         act_batch: np.array
+#             batch of actions executed given obs_batch
+#         rew_batch: np.array
+#             rewards received as results of executing act_batch
+#         next_obs_batch: np.array
+#             next set of observations seen after executing act_batch
+#         done_mask: np.array
+#             done_mask[i] = 1 if executing act_batch[i] resulted in
+#             the end of an episode and 0 otherwise.
+#         gammas: np.array
+#             product of gammas for N-step returns
+#         weights: np.array
+#             Array of shape (batch_size,) and dtype np.float32
+#             denoting importance weight of each sampled transition
+#         idxes: np.array
+#             Array of shape (batch_size,) and dtype np.int32
+#             idexes in buffer of sampled experiences
+#         """
+#         assert beta > 0
+
+#         idxes = self._sample_proportional(batch_size)
+
+#         weights = []
+#         p_min = self._it_min.min() / (self._it_sum.sum() + epsilon)
+#         max_weight = (p_min * len(self._storage)) ** (-beta)
+
+#         for idx in idxes:
+#             p_sample = self._it_sum[idx] / (self._it_sum.sum() + epsilon)
+#             weight = (p_sample * len(self._storage)) ** (-beta)
+#             weights.append(weight / (max_weight + epsilon))
+#         weights = np.array(weights)
+#         encoded_sample = self._encode_sample(idxes)
+
+#         return encoded_sample, idxes, weights
+
+#     def update_priorities(self, idxes, priorities):
+#         """Update priorities of sampled transitions.
+#         sets priority of transition at index idxes[i] in buffer
+#         to priorities[i].
+#         Parameters
+#         ----------
+#         idxes: [int]
+#             List of idxes of sampled transitions
+#         priorities: [float]
+#             List of updated priorities corresponding to
+#             transitions at the sampled idxes denoted by
+#             variable `idxes`.
+#         """
+#         assert len(idxes) == len(priorities)
+#         for idx, priority in zip(idxes, priorities):
+#             assert priority > 0
+#             assert 0 <= idx < len(self._storage)
+#             self._it_sum[idx] = priority ** self._alpha
+#             self._it_min[idx] = priority ** self._alpha
+
+#             self._max_priority = max(self._max_priority, priority)
+    
+#     def get_size(self):
+#         return len(self._storage)
+    
+#     def incorporate_buffer(self, buffer):
+#         # elements of the buffer must be appropriately sized tuples
+#         for element in buffer:
+#             self.add(*element)

Apex-DQN/segtree.py
@@ -3,36 +3,30 @@
 # https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
 """
 
-import random
-import operator
+import numpy as np
 
 class SegmentTree(object):
     def __init__(self, capacity, operation, neutral_element):
-        """Build a Segment Tree data structure.
+        """
+        Build a Segment Tree data structure.
         https://en.wikipedia.org/wiki/Segment_tree
-        Can be used as regular array, but with two
+        Can be used as regular array that supports Index arrays, but with two
         important differences:
             a) setting item's value is slightly slower.
                It is O(lg capacity) instead of O(1).
             b) user has access to an efficient ( O(log segment size) )
                `reduce` operation which reduces `operation` over
                a contiguous subsequence of items in the array.
-        Paramters
-        ---------
-        capacity: int
-            Total size of the array - must be a power of two.
-        operation: lambda obj, obj -> obj
-            and operation for combining elements (eg. sum, max)
-            must form a mathematical group together with the set of
-            possible values for array elements (i.e. be associative)
-        neutral_element: obj
-            neutral element for the operation above. eg. float('-inf')
-            for max and 0 for sum.
+        :param capacity: (int) Total size of the array - must be a power of two.
+        :param operation: (lambda (Any, Any): Any) operation for combining elements (eg. sum, max) must form a
+            mathematical group together with the set of possible values for array elements (i.e. be associative)
+        :param neutral_element: (Any) neutral element for the operation above. eg. float('-inf') for max and 0 for sum.
         """
         assert capacity > 0 and capacity & (capacity - 1) == 0, "capacity must be positive and a power of 2."
         self._capacity = capacity
         self._value = [neutral_element for _ in range(2 * capacity)]
         self._operation = operation
+        self.neutral_element = neutral_element
 
     def _reduce_helper(self, start, end, node, node_start, node_end):
         if start == node_start and end == node_end:
@@ -50,19 +44,13 @@ class SegmentTree(object):
                 )
 
     def reduce(self, start=0, end=None):
-        """Returns result of applying `self.operation`
+        """
+        Returns result of applying `self.operation`
         to a contiguous subsequence of the array.
             self.operation(arr[start], operation(arr[start+1], operation(... arr[end])))
-        Parameters
-        ----------
-        start: int
-            beginning of the subsequence
-        end: int
-            end of the subsequences
-        Returns
-        -------
-        reduced: obj
-            result of reducing self.operation over the specified range of array elements.
+        :param start: (int) beginning of the subsequence
+        :param end: (int) end of the subsequences
+        :return: (Any) result of reducing self.operation over the specified range of array elements.
         """
         if end is None:
             end = self._capacity
@@ -72,19 +60,25 @@ class SegmentTree(object):
         return self._reduce_helper(start, end, 1, 0, self._capacity - 1)
 
     def __setitem__(self, idx, val):
-        # index of the leaf
-        idx += self._capacity
-        self._value[idx] = val
-        idx //= 2
-        while idx >= 1:
-            self._value[idx] = self._operation(
-                self._value[2 * idx],
-                self._value[2 * idx + 1]
+        # indexes of the leaf
+        idxs = idx + self._capacity
+        self._value[idxs] = val
+        if isinstance(idxs, int):
+            idxs = np.array([idxs])
+        # go up one level in the tree and remove duplicate indexes
+        idxs = unique(idxs // 2)
+        while len(idxs) > 1 or idxs[0] > 0:
+            # as long as there are non-zero indexes, update the corresponding values
+            self._value[idxs] = self._operation(
+                self._value[2 * idxs],
+                self._value[2 * idxs + 1]
             )
-            idx //= 2
+            # go up one level in the tree and remove duplicate indexes
+            idxs = unique(idxs // 2)
 
     def __getitem__(self, idx):
-        assert 0 <= idx < self._capacity
+        assert np.max(idx) < self._capacity
+        assert 0 <= np.min(idx)
         return self._value[self._capacity + idx]
 
 
@@ -92,41 +86,49 @@ class SumSegmentTree(SegmentTree):
     def __init__(self, capacity):
         super(SumSegmentTree, self).__init__(
             capacity=capacity,
-            operation=operator.add,
+            operation=np.add,
             neutral_element=0.0
         )
+        self._value = np.array(self._value)
 
     def sum(self, start=0, end=None):
-        """Returns arr[start] + ... + arr[end]"""
+        """
+        Returns arr[start] + ... + arr[end]
+        :param start: (int) start position of the reduction (must be >= 0)
+        :param end: (int) end position of the reduction (must be < len(arr), can be None for len(arr) - 1)
+        :return: (Any) reduction of SumSegmentTree
+        """
         return super(SumSegmentTree, self).reduce(start, end)
 
     def find_prefixsum_idx(self, prefixsum):
-        """Find the highest index `i` in the array such that
-            sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum
+        """
+        Find the highest index `i` in the array such that
+            sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum for each entry in prefixsum
         if array values are probabilities, this function
         allows to sample indexes according to the discrete
         probability efficiently.
-        Parameters
-        ----------
-        perfixsum: float
-            upperbound on the sum of array prefix
-        Returns
-        -------
-        idx: int
-            highest index satisfying the prefixsum constraint
+        :param prefixsum: (np.ndarray) float upper bounds on the sum of array prefix
+        :return: (np.ndarray) highest indexes satisfying the prefixsum constraint
         """
-        try:
-            assert 0 <= prefixsum <= self.sum() + 1e-5
-        except AssertionError:
-            print("Prefix sum error: {}".format(prefixsum))
-            exit()
-        idx = 1
-        while idx < self._capacity:  # while non-leaf
-            if self._value[2 * idx] > prefixsum:
-                idx = 2 * idx
-            else:
-                prefixsum -= self._value[2 * idx]
-                idx = 2 * idx + 1
+        if isinstance(prefixsum, float):
+            prefixsum = np.array([prefixsum])
+        assert 0 <= np.min(prefixsum)
+        assert np.max(prefixsum) <= self.sum() + 1e-5
+        assert isinstance(prefixsum[0], float)
+
+        idx = np.ones(len(prefixsum), dtype=int)
+        cont = np.ones(len(prefixsum), dtype=bool)
+
+        while np.any(cont):  # while not all nodes are leafs
+            idx[cont] = 2 * idx[cont]
+            prefixsum_new = np.where(self._value[idx] <= prefixsum, prefixsum - self._value[idx], prefixsum)
+            # prepare update of prefixsum for all right children
+            idx = np.where(np.logical_or(self._value[idx] > prefixsum, np.logical_not(cont)), idx, idx + 1)
+            # Select child node for non-leaf nodes
+            prefixsum = prefixsum_new
+            # update prefixsum
+            cont = idx < self._capacity
+            # collect leafs
         return idx - self._capacity
 
 
@@ -134,11 +136,154 @@ class MinSegmentTree(SegmentTree):
     def __init__(self, capacity):
         super(MinSegmentTree, self).__init__(
             capacity=capacity,
-            operation=min,
+            operation=np.minimum,
             neutral_element=float('inf')
         )
+        self._value = np.array(self._value)
 
     def min(self, start=0, end=None):
-        """Returns min(arr[start], ...,  arr[end])"""
-
+        """
+        Returns min(arr[start], ...,  arr[end])
+        :param start: (int) start position of the reduction (must be >= 0)
+        :param end: (int) end position of the reduction (must be < len(arr), can be None for len(arr) - 1)
+        :return: (Any) reduction of MinSegmentTree
+        """
         return super(MinSegmentTree, self).reduce(start, end)
+
+
+# class SegmentTree(object):
+#     def __init__(self, capacity, operation, neutral_element):
+#         """Build a Segment Tree data structure.
+#         https://en.wikipedia.org/wiki/Segment_tree
+#         Can be used as regular array, but with two
+#         important differences:
+#             a) setting item's value is slightly slower.
+#                It is O(lg capacity) instead of O(1).
+#             b) user has access to an efficient ( O(log segment size) )
+#                `reduce` operation which reduces `operation` over
+#                a contiguous subsequence of items in the array.
+#         Paramters
+#         ---------
+#         capacity: int
+#             Total size of the array - must be a power of two.
+#         operation: lambda obj, obj -> obj
+#             and operation for combining elements (eg. sum, max)
+#             must form a mathematical group together with the set of
+#             possible values for array elements (i.e. be associative)
+#         neutral_element: obj
+#             neutral element for the operation above. eg. float('-inf')
+#             for max and 0 for sum.
+#         """
+#         assert capacity > 0 and capacity & (capacity - 1) == 0, "capacity must be positive and a power of 2."
+#         self._capacity = capacity
+#         self._value = [neutral_element for _ in range(2 * capacity)]
+#         self._operation = operation
+
+#     def _reduce_helper(self, start, end, node, node_start, node_end):
+#         if start == node_start and end == node_end:
+#             return self._value[node]
+#         mid = (node_start + node_end) // 2
+#         if end <= mid:
+#             return self._reduce_helper(start, end, 2 * node, node_start, mid)
+#         else:
+#             if mid + 1 <= start:
+#                 return self._reduce_helper(start, end, 2 * node + 1, mid + 1, node_end)
+#             else:
+#                 return self._operation(
+#                     self._reduce_helper(start, mid, 2 * node, node_start, mid),
+#                     self._reduce_helper(mid + 1, end, 2 * node + 1, mid + 1, node_end)
+#                 )
+
+#     def reduce(self, start=0, end=None):
+#         """Returns result of applying `self.operation`
+#         to a contiguous subsequence of the array.
+#             self.operation(arr[start], operation(arr[start+1], operation(... arr[end])))
+#         Parameters
+#         ----------
+#         start: int
+#             beginning of the subsequence
+#         end: int
+#             end of the subsequences
+#         Returns
+#         -------
+#         reduced: obj
+#             result of reducing self.operation over the specified range of array elements.
+#         """
+#         if end is None:
+#             end = self._capacity
+#         if end < 0:
+#             end += self._capacity
+#         end -= 1
+#         return self._reduce_helper(start, end, 1, 0, self._capacity - 1)
+
+#     def __setitem__(self, idx, val):
+#         # index of the leaf
+#         idx += self._capacity
+#         self._value[idx] = val
+#         idx //= 2
+#         while idx >= 1:
+#             self._value[idx] = self._operation(
+#                 self._value[2 * idx],
+#                 self._value[2 * idx + 1]
+#             )
+#             idx //= 2
+
+#     def __getitem__(self, idx):
+#         assert 0 <= idx < self._capacity
+#         return self._value[self._capacity + idx]
+
+
+# class SumSegmentTree(SegmentTree):
+#     def __init__(self, capacity):
+#         super(SumSegmentTree, self).__init__(
+#             capacity=capacity,
+#             operation=operator.add,
+#             neutral_element=0.0
+#         )
+
+#     def sum(self, start=0, end=None):
+#         """Returns arr[start] + ... + arr[end]"""
+#         return super(SumSegmentTree, self).reduce(start, end)
+
+#     def find_prefixsum_idx(self, prefixsum):
+#         """Find the highest index `i` in the array such that
+#             sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum
+#         if array values are probabilities, this function
+#         allows to sample indexes according to the discrete
+#         probability efficiently.
+#         Parameters
+#         ----------
+#         perfixsum: float
+#             upperbound on the sum of array prefix
+#         Returns
+#         -------
+#         idx: int
+#             highest index satisfying the prefixsum constraint
+#         """
+#         try:
+#             assert 0 <= prefixsum <= self.sum() + 1e-5
+#         except AssertionError:
+#             print("Prefix sum error: {}".format(prefixsum))
+#             exit()
+#         idx = 1
+#         while idx < self._capacity:  # while non-leaf
+#             if self._value[2 * idx] > prefixsum:
+#                 idx = 2 * idx
+#             else:
+#                 prefixsum -= self._value[2 * idx]
+#                 idx = 2 * idx + 1
+#         return idx - self._capacity
+
+
+# class MinSegmentTree(SegmentTree):
+#     def __init__(self, capacity):
+#         super(MinSegmentTree, self).__init__(
+#             capacity=capacity,
+#             operation=min,
+#             neutral_element=float('inf')
+#         )
+
+#     def min(self, start=0, end=None):
+#         """Returns min(arr[start], ...,  arr[end])"""
+
+#         return super(MinSegmentTree, self).reduce(start, end)