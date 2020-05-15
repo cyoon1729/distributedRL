5b6185438d232bcbb56d753e75b8a11dcb017415
chris.yoon <chris.yoon@medipixel.io>
2020-05-15 17:53:28 +0900

    skeleton for distributed architecture

--- common/abstract/architecture.py
+++ common/abstract/architecture.py
@@ -1,33 +1,56 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
-from learner import Learner
-from worker import Worker
-from distributedRL.
+from distributedRL.common.abstract.learner import Learner
+from distributedRL.common.abstract.worker import Worker
+from distributedRL.common.utils.buffers import Buffer
+from distributedRL.common.utils.param_server import ParamServer
+
 
 class Architecture(ABC):
     """Abstract Architecture used for all distributed architectures
     
     Attributes:
-        self.num_workers =
+        self.num_workers = 
     
     """
 
     def __init__(self, cfg: dict):
-
+        """Initialize"""
         self.cfg = cfg
-        self.num_workers = cfg['num_workers'] 
-        self.num_learners = cfg['num_learners']
+        self.env = self.cfg['env']
+        self.num_workers = self.cfg['num_workers'] 
+        self.num_learners = self.cfg['num_learners']
+        self.num_step = self.cfg['num_step']
 
-    
-    def spawn(self, worker: type, learner: type, param_server: ParameterServer, centralized_buffer: Buffer):
-        
-        self.workers
-
-    
+        self.initial_brain = None
+        self.initial_worker_brain = None
 
     @abstractmethod
-    def train(self):
-        pass  
+    def initialize_central_brain(self):
+        """Initialize brain"""
+        pass
 
+    def spawn(self, Worker: type, Learner: type, param_server: ParamServer, centralized_buffer: Buffer):    
+        """Spawn Components of Distributed Architecture"""
+        assert self.initial_brain is None and self.initial_worker_brain is None
+
+        self.learner = Learner(self.initial_brain, self.cfg)
+        self.worker_seeds = np.random.choice(
+            np.arange(1, 999, 1), self.num_workers
+        )
+        self.cfg['worker_seeds'] = self.worker_seeds.tolist()
+        self.workers = [
+            Worker.remote(worker_id, self.initial_worker_brain, self.env, seed, self.cfg) for seed, worker_id in zip(
+                self.worker_seeds,
+                len(self.worker_seeds)
+            )
+        ]
+        self.param_server = param_server
+        self.centralized_buffer = centralized_buffer
+
+    @abstractmethod
+    def run(self):
+        """Run main training loop"""
+        pass  
 
