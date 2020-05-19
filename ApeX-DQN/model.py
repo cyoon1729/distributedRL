c488165e99c7dd815948b4528b0772de46a2594f
chris.yoon <chris.yoon@medipixel.io>
2020-05-19 15:45:24 +0900

    debug dqn workers

--- ApeX-DQN/model.py
+++ ApeX-DQN/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-
+import torch.nn.functional as F
 
 class ConvDuelingDQN(nn.Module):
 
@@ -9,16 +9,16 @@ class ConvDuelingDQN(nn.Module):
         super(ConvDuelingDQN, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.fc_input_dim = self.feature_size()
-        
         self.conv = nn.Sequential(
-            nn.Conv2d(input_dim[0], 32, kernel_size=8, stride=4),
+            nn.Conv2d(self.input_dim[0], 32, kernel_size=8, stride=4),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
         )
+
+        self.fc_input_dim = self.feature_size()
 
         self.value_stream = nn.Sequential(
             nn.Linear(self.fc_input_dim, 128),
@@ -32,6 +32,7 @@ class ConvDuelingDQN(nn.Module):
             nn.Linear(128, self.output_dim)
         )
 
+
     def forward(self, state):
         features = self.conv(state)
         features = features.view(features.size(0), -1)
@@ -42,7 +43,43 @@ class ConvDuelingDQN(nn.Module):
         return qvals
 
     def feature_size(self):
-        return self.conv(torch.FloatTensor((torch.zeros(1, *self.input_dim)))).view(1, -1).size(1)
+        return self.conv(torch.zeros(1, *self.input_dim)).view(1, -1).size(1)
+
+class ConvDQN(nn.Module):
+    
+    def __init__(self, num_inputs, num_actions, hidden_size=256, init_w=3e-3):
+        super(ConvDQN, self).__init__()
+        self.num_inputs = num_inputs
+        self.num_actions = num_actions
+        self.conv = nn.Sequential(
+            nn.Conv2d(num_inputs[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.fc_input_dim = self.feature_size()
+        self.linear1 = nn.Linear(self.fc_input_dim, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, num_actions)
+
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state):
+        features = self.conv(state)
+        features = features.view(features.size(0), -1)
+        
+        x = F.relu(self.linear1(features))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+
+        return x
+
+    def feature_size(self):
+        return self.conv(torch.zeros(1, *self.num_inputs)).view(1, -1).size(1)
 
 
 class DuelingDQN(nn.Module):
---
+++ ApeX-DQN/test.py
@@ -0,0 +1,61 @@
+import sys
+sys.path.append('../')
+
+import ray
+import torch
+from worker_learner import DQNWorker, DQNLearner
+from common.utils.utils import read_config, preprocess_nstep
+from model import  ConvDuelingDQN, ConvDQN
+from common.utils.baseline_wrappers import make_atari, wrap_deepmind, wrap_pytorch
+from common.utils.buffer_helper import BufferHelper
+from common.utils.buffer import PrioritizedReplayBuffer
+from datetime import datetime
+
+ray.init()
+
+config_pth = "config.yml"
+cfg = read_config("config.yml")
+dqn = ConvDuelingDQN(cfg['obs_dim'], cfg['action_dim'])
+tdqn = ConvDuelingDQN(cfg['obs_dim'], cfg['action_dim'])
+#dqn = ConvDQN(cfg['obs_dim'], cfg['action_dim'])
+# print(cfg['obs_dim'], cfg['action_dim'])
+# if cfg['atari'] is True:
+#     env = make_atari(cfg['env'])
+#     env = wrap_deepmind(env)
+#     env = wrap_pytorch(env)
+
+worker = DQNWorker.remote(1, dqn, cfg['env'], 1, cfg)
+brains = (dqn, tdqn)
+learner = DQNLearner.remote(brains, cfg)
+
+
+
+buffer_max_size = cfg['buffer_max_size']
+priority_beta = cfg['priority_beta']
+priority_alpha = cfg['priority_alpha']
+priority_beta_start = cfg['priority_beta_start']
+priority_beta_end = cfg['priority_beta_end']
+num_train_steps = cfg['max_num_updates']
+priority_beta_increment = ((priority_beta_end - priority_beta_start)
+                            / num_train_steps)
+priority_alpha = cfg['priority_alpha']
+batch_size = cfg['batch_size']
+
+global_buffer = BufferHelper.remote(
+        	PrioritizedReplayBuffer(buffer_max_size, priority_alpha)
+)
+
+if __name__ == "__main__":
+    worker.collect_data.remote()
+    wbuffer = ray.get(
+        worker.get_buffer.remote()
+    )
+    global_buffer.incorporate_new_data.remote(
+        wbuffer
+    )
+    batch = ray.get(
+        global_buffer.sample_data.remote(batch_size, priority_beta)
+    )
+    learner.learning_step.remote(batch)
+    # print(batch)
+    print("done")
