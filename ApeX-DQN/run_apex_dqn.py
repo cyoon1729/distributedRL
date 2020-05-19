06b2b00bcff974d72978ae6563a34ee7840e083a
chris.yoon <chris.yoon@medipixel.io>
2020-05-19 17:46:08 +0900

    implemen Ape-X DQN

---
+++ ApeX-DQN/run_apex_dqn.py
@@ -0,0 +1,19 @@
+import ray
+
+from common.utils.utils import read_config
+from architectures.apex import ApeX
+from worker_learner import DQNWorker, DQNLearner
+from model import ConvDuelingDQN, ConvDQN
+
+ray.init()
+
+
+if __name__ == "__main__":
+    cfg = read_config("config.yml")
+
+    dqn = ConvDuelingDQN(cfg["obs_dim"], cfg["action_dim"])
+    target_dqn = ConvDuelingDQN(cfg["obs_dim"], cfg["action_dim"])
+    brain = (dqn, target_dqn)
+
+    ApeXDQN = ApeX(cfg)
+    ApeXDQN.spawn(DQNWorker, DQNLearner, brain)
