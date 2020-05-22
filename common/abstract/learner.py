23fd7b12176f9f152b3b3edc162ca22da1111b2b
chris.yoon <chris.yoon@medipixel.io>
2020-05-22 18:51:38 +0900

    add zmq based communication for all actors (no debug)

--- common/abstract/learner.py
+++ common/abstract/learner.py
@@ -5,14 +5,26 @@ from typing import Union
 import numpy as np
 import torch
 import torch.nn as nn
+import pyarrow as pa
+import zmq
 
-
+@ray.remote(num_gpus=1)
 class Learner(ABC):
-    def __init__(self, brain: Union[nn.Module, tuple], cfg: dict):
-        self.cfg = cfg
-        self.device = torch.device(cfg["learner_device"])
+    def __init__(self, brain: Union[nn.Module, tuple], learner_cfg: dict, comm_cfg: dict):
+        self.cfg = learner_cfg
+        self.device = torch.device(self.learner_cfg["learner_device"])
         self.brain = deepcopy(brain)
+        
         self.gamma = self.cfg["gamma"]
+        self.param_update_interval = self.cfg["param_update_interval"]
+
+        # unpack communication configs
+        self.repreq_port = comm_cfg["repreq_port"]
+        self.pubsub_port = comm_cfg["pubsub_port"]
+
+        # initialize zmq sockets
+        print("[Learner]: initializing sockets..")
+        self.initialize_sockets()
 
     @abstractmethod
     def write_log(self):
@@ -23,7 +35,7 @@ class Learner(ABC):
         pass
 
     @abstractmethod
-    def get_params(self):
+    def get_params(self) -> np.ndarray:
         """Return model params for synchronization"""
         pass
 
@@ -38,3 +50,40 @@ class Learner(ABC):
         for param in list(state_dict):
             params.append(state_dict[param])
         return params
+
+    def initialize_sockets(self):
+        # For sending new params to workers
+        context = zmq.Context()
+        self.pub_socket = context.socket(zmq.PUB)
+        self.pub_socket.bind(f"tcp://127.0.0.1:{self.pubsub_port}")
+
+        # For receiving batch from, sending new priorities to Buffer # write another with PUSH/PULL for non PER version
+        context = zmq.Context()
+        self.rep_socket = context.socket(zmq.REP)
+        self.rep_socket.bind(f"tcp://127.0.0.1:{self.repreq_port}")
+    
+    def publish_params(self, new_params: np.ndarray):
+        new_params_id = pa.serialize(new_params).to_buffer()
+        self.pub_socket.send(new_params_id)
+    
+    def recv_replay_data_(self):
+        replay_data_id = self.rep_socket.recv()
+        replay_data = pa.deserialize(replay_data_id)
+        return replay_data
+
+    def send_new_priorities(self, idxes: np.ndarray, priorities: np.ndarray):
+        new_priors = [idxes, priorities]
+        new_priors_id = pa.serialize(new_priors).to_buffer()
+        self.rep_socket.send(new_priors_id)
+    
+    def run(self):
+        self.update_step = 0
+        while True:
+            replay_data = self.recv_replay_data_()
+            step_info, idxes, priorities = self.learning_step(replay_data)
+            self.update_step = self.update_step + 1
+            self.send_new_priorities(idxes, priorities)
+
+            if self.update_step % self.param_update_interval == 0:
+                params = self.get_params()
+                self.publish_params(params)
--- common/utils/buffer_helper.py
+++ common/utils/buffer_helper.py
@@ -1,19 +1,69 @@
 from typing import Deque
 
 import ray
-
+import zmq
+import pyarrow as pa
 
 @ray.remote
-class BufferHelper(object):
-    def __init__(self, buffer):
+class PrioritizedReplayBufferHelper(object):
+    def __init__(self, buffer, buffer_cfg: dict, comm_cfg: dict):
         self.buffer = buffer
+        self.cfg = buffer_cfg
+        
+        # unpack buffer configs
+        self.max_num_updates = self.cfg["max_num_updates"]
+        self.priority_alpha = self.cfg["priority_alpha"]
+        self.priority_beta = self.cfg["priority_beta_start"]
+        self.priority_beta_end = self.cfg["priority_beta_end"]
+        self.priority_beta_increment = (
+            self.priority_beta_end - self.priority_beta
+        ) / self.max_num_updates
 
-    def incorporate_new_data(self, new_data: Deque):
-        for data in new_data:
-            self.buffer.add(*data)
+        self.batch_size = self.cfg["batch_size"]
 
-    def sample_data(self, batch_size: int, priority_beta):
-        return self.buffer.sample(batch_size, priority_beta)
+        # unpack communication configs
+        self.repreq_port = comm_cfg["repreq_port"]
+        self.pullpush_port = comm_cfg["pullpush_port"]
+
+        # initialize zmq sockets
+        print("[Buffer]: initializing sockets..")
+        self.initialize_sockets()
+
+    def initialize_sockets(self):
+        # for sending batch to learner and retrieving new priorities
+        context = zmq.Context()
+        self.rep_socket = context.socket(zmq.REQ)
+        self.rep_socket.connect(f"tcp://127.0.0.1:{self.repreq_port}")
+
+        # for receiving replay data from workers
+        context = zmq.Context()
+        self.pull_socket = context.socket(zmq.PULL)
+        self.pull_socket.connect(f"tcp://127.0.0.1:{self.pullpush_port}")
     
-    def update_priorities(self, idxes, new_priorities):
+    def send_batch_recv_priors(self)
+        # send batch and request priorities (blocking recv)
+        batch = self.buffer.sample(self.batch_size, self.priority_beta)
+        batch_id = pa.serialize(batch).to_buffer()
+        self.rep_socket.send(batch_id) 
+
+        # receive and update priorities 
+        new_priors_id = self.rep_socket.recv()
+        idxes, new_priorities = pa.deserialize(new_priors_id)
         self.buffer.update_priorities(idxes, new_priorities)
+    
+    def recv_data(self):
+        new_replay_data_id = self.pull_socket.recv()
+        if new_replay_data_id:
+            new_replay_data = pa.deserialize(new_replay_data_id)
+            for data in new_replay_data:
+                self.buffer.add(*data)
+        else:
+            pass
+    
+    def run(self):
+        while True:
+            self.recv_data()
+            if len(self.buffer) > self.batch_size
+                self.send_batch_recv_priors()    
+            else:
+                pass
